179b80cc09eb705f6a6c7eb7279d5c7ed3dc4b84
Oneul-hyeon <gusxo3975@naver.com>
2024-04-09 12:31:40 +0900

    Modified demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,27 @@
 import gradio as gr
 from evaluate import get_summary
+from test_info import get_info
+from rouge import Rouge
 
 def ext_summarization(text) :
     result = get_summary(text, 3)
-    return result
+    label = get_info(text)
 
-demo = gr.Interface(fn=ext_summarization, inputs = gr.Textbox(), outputs = gr.Textbox(), title = "추출 요약")
+    rouge = Rouge()
+    rouge_score = rouge.get_scores(result, label, avg = True)
+
+    return_rouge = f""
+    for key in rouge_score :
+        value = rouge_score[key]
+        return_rouge += f"{key.upper()}\n"
+        return_rouge += f"     > Precision : {value['p']:.2f}\n"
+        return_rouge += f"     > Recall : {value['r']:.2f}\n"
+        return_rouge += f"     > F-measure : {value['f']:.2f}\n"
+    return result, label, return_rouge[:-2]
+
+demo = gr.Interface(fn=ext_summarization,
+                    inputs = gr.Textbox(),
+                    outputs = [gr.Textbox(label = "추출 요약"), gr.Textbox(label = "정답 요약"), gr.Textbox(label = "Rouge Score")],
+                    title = "추출 요약")
+
 demo.launch(server_name = "0.0.0.0", server_port = 11021)
